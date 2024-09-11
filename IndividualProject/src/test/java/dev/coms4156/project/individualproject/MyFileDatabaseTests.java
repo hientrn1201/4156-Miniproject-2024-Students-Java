3f3eba013e9f091a82d656798afc98c0dbe9669e
Hien Tran <85942569+hientrn1201@users.noreply.github.com>
2024-09-11 11:35:02 -0400

    chore: add testSaveContentsToFile to MyFileDatabaseTests

--- IndividualProject/src/test/java/dev/coms4156/project/individualproject/MyFileDatabaseTests.java
+++ IndividualProject/src/test/java/dev/coms4156/project/individualproject/MyFileDatabaseTests.java
@@ -1,7 +1,12 @@
 package dev.coms4156.project.individualproject;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -35,5 +40,27 @@ class MyFileDatabaseTests {
     assertTrue(result.contains("COMS"));
     assertTrue(result.contains("IEOR"));
     assertTrue(result.contains("PSYC"));
+  }
+
+  /**
+   * Tests the saveContentsToFile method.
+   */
+  @Test
+  void testSaveContentsToFile() throws IOException {
+    String testFilePath = "./test_data.txt";
+    MyFileDatabase myFileDatabaseClone = new MyFileDatabase(1, testFilePath);
+    myFileDatabaseClone.setMapping(myFileDatabase.getDepartmentMapping());
+
+    myFileDatabaseClone.saveContentsToFile();
+
+    File savedFile = new File(testFilePath);
+    assertTrue(savedFile.exists(), "File should be created");
+
+    String fileContentClone = new String(Files.readAllBytes(Paths.get(testFilePath)));
+    String fileContent = new String(Files.readAllBytes(Paths.get("./data.txt")));
+    assertEquals(fileContent, fileContentClone);
+
+    // Clean up: delete the test file
+    assertTrue(savedFile.delete());
   }
 }
